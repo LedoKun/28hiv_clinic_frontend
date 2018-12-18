786cc4cf588275ee4268d739d2b1cc4a753b1f9b
LedoKun <romamp100@gmail.com>
2018-12-18 03:34:47 +0000

    Add more stats

---
+++ src/components/Data/Table.vue
@@ -0,0 +1,57 @@
+<template>
+<b-collapse class="card">
+    <div slot="trigger" slot-scope="props" class="card-header">
+        <p class="card-header-title">
+            {{ header }}
+        </p>
+        <a class="card-header-icon">
+            <b-icon
+                :icon="props.open ? 'menu-down' : 'menu-up'">
+            </b-icon>
+        </a>
+    </div>
+
+    <div class="card-table">
+        <div class="table-wrapper" v-html="data">
+        </div>
+    </div>
+
+</b-collapse>
+</template>
+
+<script>
+export default {
+    name: 'Table',
+    props: {
+        data: {
+            default: null
+        },
+        header: {
+            type: String
+        }
+    }
+}
+</script>
+
+<style scoped>
+.card {
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.18);
+  margin-bottom: 2rem;
+}
+.card-header-title {
+  color: #8F99A3;
+  font-weight: 400;
+}
+.card .content {
+  font-size: 14px;
+}
+.table {
+    width: 98%;
+    margin-left:auto; 
+    margin-right:auto;
+    margin-top: 2px;
+}
+.card-table {
+  overflow-y: auto;
+}
+</style>
--- src/views/Data.vue
+++ src/views/Data.vue
@@ -5,16 +5,113 @@
             <AllPatients />
         </div>
     </div>
+
+    <div class="columns" v-if="isReady">
+        <div class="column">
+            <Table
+                :header="'Sex-Gender-Age'"
+                :data="tables.df_groupby_age"
+            />
+        </div>
+
+        <div class="column">
+            <Table
+                :header="'Sex-Gender-Age (Thais)'"
+                :data="tables.df_thais_groupby_age"
+            />
+        </div>
+
+        <div class="column">
+            <Table
+                :header="'Sex-Gender-Age (Non-Thai)'"
+                :data="tables.df_non_thai_groupby_age"
+            />
+        </div>
+    </div>
+
+    <div class="columns" v-if="isReady">
+        <div class="column">
+            <Table
+                :header="'สิทธิ์การรักษา (คนไทย)'"
+                :data="tables.df_thais_groupby_payer"
+            />
+        </div>
+        
+        <div class="column">
+            <Table
+                :header="'สิทธิ์การรักษา (คนต่างชาติ)'"
+                :data="tables.df_non_thai_groupby_payer"
+            />
+        </div>
+    </div>
+
+    <div class="columns" v-if="isReady">
+        <div class="column">
+            <Table
+                :header="'Referral Status'"
+                :data="tables.df_isRefer"
+            />
+        </div>
+    </div>
+
+    <div class="columns" v-if="isReady">
+        <div class="column">
+            <Table
+                :header="'ผู้ป่วยรายใหม่'"
+                :data="tables.df_new_patient_by_months"
+            />
+        </div>
+
+        <div class="column">
+            <Table
+                :header="'จำนวนครั้งที่ผู้ป่วยที่มาพบแพทย์ทั้งหมด'"
+                :data="tables.df_all_visits_by_months"
+            />
+        </div>
+
+    </div>
+
 </div>
 </template>
 
 <script>
 import AllPatients from '../components/Data/AllPatients'
+import Table from '../components/Data/Table'
+import instance from '@/utils/http'
 
 export default {
     name: 'Data',
     components: {
-        AllPatients
+        AllPatients,
+        Table
+    },
+    mounted() {
+        this.fetchData()
+    },
+    methods: {
+        async fetchData() {
+            let self = this
+
+            self.isReady = false
+
+            try {
+                let response = await instance({
+                    url: '/api/data/stats',
+                    method: 'get'
+                })
+
+                self.tables = response.data
+                self.isReady = true
+            } catch (error) {
+                // error
+            }
+        }
+    },
+    data: function () {
+        return {
+            tables: null,
+            isReady: false
+        }
     }
 }
 </script>
